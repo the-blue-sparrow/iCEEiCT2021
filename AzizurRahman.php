3702a89fcf7fbdd3d07f14df6d4ad99dfcdd5a3e
syma80 <syma80@users.noreply.github.com>
2021-03-30 20:43:49 +0600

    30mar 8.43pm

---
+++ AzizurRahman.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+
+  
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Welcome - ICEEICT 2018</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+
+  <!-- Bootstrap Core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,500,300,900' rel='stylesheet' type='text/css'>
+
+  <!-- Custom CSS -->
+  <link href="css/modern-business.css" rel="stylesheet">
+
+
+
+</head>
+
+<body>
+
+
+  <!-- Header Large -->
+  <div class="container logobar visible-lg">
+
+    <div class="col-md-1 text-left">
+      <a href="index-2.html"><img src="images/logo.png"> </a>
+    </div>
+
+    <div class="col-md-6 text-left" style="padding-top:10px">
+      <h1 class="brand_slogan"></h1>
+      <p class="brand_name"></p>
+    </div>
+
+    <div class="col-md-4 text-right" style="padding-top:10px">
+     
+      <a href="http://www.MIST.ac.bd/" target="_blank">
+        <p class="venue">MILITARY INSTITUTE OF SCIENCE AND TECHNOLOGY</p>
+    </div>
+    <div class="col-md-1 text-right">
+      <img src="images/mist_logo.jpg"></a>
+    </div>
+  </div>
+
+
+  <!-- Header Medium -->
+  <div class="container logobar visible-md">
+    <div class="col-md-1 text-left">
+      <a href="index-2.html"><img src="images/logo.png"> </a>
+    </div>
+    <div class="col-md-6 text-left" style="padding-left:25px;padding-top:10px">
+      <h1 class="brand_slogan">4th International Conference on </h1>
+      <p class="brand_name_small">Electrical Engineering and Information & Communication Technolog</p>
+    </div>
+    <div class="col-md-4 text-right" style="padding-top:10px">
+      <h1 class="date">September 13-15</h1>
+      <a href="http://www.MIST.ac.bd/" target="_blank">
+        <p class="venue">MILITARY INSTITUTE OF SCIENCE AND TECHNOLOGY</p>
+    </div>
+    <div class="col-md-1 text-right">
+      <img src="images/mist_logo.jpg"></a>
+    </div>
+  </div>
+
+
+  <!-- Header Small -->
+  <div class="container logobar hidden-md hidden-lg">
+    <div class="col-md-1 text-center">
+      <a href="index-2.html"><img src="images/logo.png" width="40" style="padding-right:5px"></a>
+      <a href="http://www.MIST.ac.bd/" target="_blank">
+	        <img src="images/mist_logo.jpg" width="40" style="padding-left:5px">
+        </a>
+    </div>
+    <div class="col-md-6 text-center" style="padding-left:25px">
+      <h1 class="brand_slogan_small">4th International Conference on </h1>
+      <p class="brand_name_small">Electrical Engineering and Information & Communication Technolog</p>
+    </div>
+    <div class="col-md-4 text-center">
+      <h1 class="date">September 13-15</h1>
+      <a href="http://www.MIST.ac.bd/" target="_blank">
+        <p class="venue">Military Institute of Science and Technology</p>
+      </a>
+    </div>
+    <div class="col-md-1 text-right">
+
+    </div>
+  </div>
+
+  <!-- Navigation -->
+  <?php require_once "topmenu.php"; ?>
+    
+    
+  <div class="container-fluid" style="background-image: url('images/bg2.jpg');background-size: cover;">
+    <div class="container">
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    
+ 
+ 
+ 
+  <br><br><br>
+  <!-- Page Content -->
+  <div class="container">
+
+
+    <!-- /.row -->
+
+    <!-- Content Row -->
+    <div class="row">
+
+        <h2 id="scope">Keynote Speaker</h2>
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="images/members/azizur.jpg" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-black">
+            <h2 style="color:#fff;">Professor B. M. Azizur Rahman</h2>
+          </div>
+        </div>
+          <br>
+          <br>
+          <br>
+          
+          
+
+        <div class="w3-container">
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>
+            
+             Professor of Photonics,Department of Electrical & Electronic Engineering
+            </p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>City University of London, Northampton Square, London EC1V 0HB, UK</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>b.m.a.rahman@city.ac.uk </p>
+          <hr>
+
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Biography</h2>
+        <div class="w3-container">
+          
+            <p>
+               Prof. B. M. Azizur Rahman received the B.Sc. Eng. and M.Sc. Eng. degrees in Electrical Engineering with distinctions from Bangladesh University of Engineering and Technology (BUET), Dhaka, Bangladesh, in 1976 and 1979, respectively. He also received two gold medals for being the best undergraduate and graduate students of the university in 1976 and 1979, respectively. In 1979, he was awarded with a Commonwealth Scholarship to study for a PhD degree in the UK and subsequently in 1982 received his PhD degree in Electronics from University College London. In 1988, he joined City University, London, as a lecturer and became a full Professor in 2000. At City University, he leads a large research group on Photonics Modelling. So far, 31 PhD students have completed their PhD degrees under his direct supervision. He has received research more than $ 15 million in research grants, published over 550 journal and conference papers, and his journal papers have been cited more than 4700 times with an h-index value 33. Prof. Rahman is a Fellow of the IEEE, the Optical Society of America, and the SPIE.  
+  
+        </p>
+            
+          <hr>
+        </div>
+      </div>
+
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Design and Optimisation of New Generation of Compact Photonic Sensors </h2>
+        <h4 class="w3-text-grey w3-padding-16">Abstract</h4>
+        <div class="w3-container">
+          
+<p>Although optical sensors using side polished or etched optical fibres, metal coated for plasmonic resonances, and incorporating Bragg grating are now sufficiently mature and well established in the market, however, designs based on more exotic nanowires and photonic crystal fibres are becoming increasingly important and showing much improved sensitivity by accessing a larger evanescent field. Similarly, novel planar design concepts, such as the silicon slot guide-based design is showing even greater promise, allowing the exploitation of well-developed CMOS fabrication technologies for potentially low-cost sensor elements. In compact Integrated Optic format, dielectric slots, plasmonic slots, Mach-Zehnder interferometer, and ring resonators are also emerging as potential optical sensors of the future.
+However, high index contrast also makes the modes in such guided-wave structures fully hybrid in nature and in such a case, full-vectorial rigorous numerical approaches will be necessary for their design optimisation. Some selected results for silicon based compact photonic sensors will be presented illustrating the value and potential of the computationally efficient finite element method in such designs.  
+The development of slot waveguides has created new opportunities for exotic optical sensor by guiding light in low index sensing area, such as gases or fluids in the slot region and thus creating the potential for more advanced, sensitive yet very compact and low-cost photonic sensors. The design and optimisation of such novel vertical slots, horizontal slots and polarization independent cross-slots for biosensing will be presented. The design of a novel plasmonic slot supporting complex plasmonic modes for gas sensing will also be presented. The design optimisation of metal coated silica nanowire for bio sensing and doped ZnO coated gold nanowire and vertical slot for gas sensing and a resonating straight slots for biosensing will also be presented.
+</p>
+          <hr>
+        </div>
+        </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+
+      </div>
+
+
+
+
+    </div>
+
+    <!-- /.row -->
+
+
+    <!-- Footer -->
+
+
+  </div>
+
+
+  <br><br>
+  <div class="container-fluid" style="background-color:#1b9c00">
+    <div class="container">
+    <footer>
+        <div class="row">
+          <div class="col-lg-6">
+
+            <p class="footer_text" style="color:white;">Copyright @ ICEEICT-2018</p>
+          </div>
+          <div class="col-lg-5 text-right">
+
+            <p class="footer_text" style="color:white;">Hosted By <a style="color:white;" href="http://www.MIST.ac.bd/" target="_blank">Military Institute of Science and Technology</a></p>
+          </div>
+        </div>
+      </footer>
+
+
+    </div>
+  </div>
+  <!-- /.container -->
+
+  <!-- jQuery -->
+  <script src="js/jquery.js"></script>
+
+  <!-- Bootstrap Core JavaScript -->
+  <script src="js/bootstrap.min.js"></script>
+  <!--Script to activate Scrolling text-->
+  <script src="js/scroll.js"></script>
+
+
+  <!-- Script to Activate the Carousel -->
+
+</body>
+
+
+
+</html>
